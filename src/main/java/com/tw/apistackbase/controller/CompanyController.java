5604904fb5447983ebea9f11d98b355fab211047
PASTEMA <martinpastema@gmail.com>
2019-10-16 15:59:22 +0800

    PutMapping Finished[Martin&Aaron]

--- src/main/java/com/tw/apistackbase/controller/CompanyController.java
+++ src/main/java/com/tw/apistackbase/controller/CompanyController.java
@@ -5,28 +5,39 @@ import com.tw.apistackbase.repository.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
 @RestController
 @RequestMapping("/companies")
 public class CompanyController {
     @Autowired
     CompanyRepository companyRepository;
 
-    @GetMapping(produces = {"application/json"})
+    @GetMapping(produces = {APPLICATION_JSON_VALUE})
     public Iterable<Company> list() {
         return  companyRepository.findAll();
     }
 
-    @GetMapping(value = "/{value}",produces = {"application/json"})
+    @GetMapping(value = "/{value}",produces = {APPLICATION_JSON_VALUE})
     public Company getCompanyByName(@PathVariable String name) {
         return  companyRepository.findOneByName(name);
     }
 
-    @PostMapping(produces = {"application/json"})
+    @PostMapping(produces = {APPLICATION_JSON_VALUE})
     public Company add(@RequestBody Company company) {
         return companyRepository.save(company);
     }
 
-    @DeleteMapping(value = "/{id}",produces = {"application/json"})
+    @PutMapping(value = "/{id}",produces = {APPLICATION_JSON_VALUE})
+    public Company updateCompany(@RequestBody Company company, @PathVariable Long id){
+        companyRepository.findById(id).ifPresent(value -> company.setId(value.getId()));
+        return companyRepository.save(company);
+    }
+
+
+    @DeleteMapping(value = "/{id}",produces = {APPLICATION_JSON_VALUE})
     public String deleteCompany(@PathVariable Long id) {
         companyRepository.deleteById(id);
         return  "Deleted";
